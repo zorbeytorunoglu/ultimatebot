b0fcdd49538989acaefcc3cf0addee9b87405a62
zorbeytorunoglu <thelegend.mcturk@gmail.com>
2022-06-04 05:43:27 +0100

    Added DataHandler

--- src/main/java/com/zorbeytorunoglu/ultimatebot/Bot.java
+++ src/main/java/com/zorbeytorunoglu/ultimatebot/Bot.java
@@ -1,24 +1,27 @@
 package com.zorbeytorunoglu.ultimatebot;
 
 import com.zorbeytorunoglu.ultimatebot.configuration.commands.CommandsHandler;
+import com.zorbeytorunoglu.ultimatebot.configuration.datas.DataHandler;
 import com.zorbeytorunoglu.ultimatebot.configuration.messages.MessageHandler;
 import com.zorbeytorunoglu.ultimatebot.configuration.settings.SettingsHandler;
 import com.zorbeytorunoglu.ultimatebot.permissions.PermissionHandler;
 import net.dv8tion.jda.api.JDA;
 
 public class Bot {
-
     private final SettingsHandler settingsHandler;
     private final PermissionHandler permissionHandler;
     private final MessageHandler messageHandler;
     private final CommandsHandler commandsHandler;
+    private final DataHandler dataHandler;
     private JDA jda;
 
-    public Bot(SettingsHandler settingsHandler, PermissionHandler permissionHandler, MessageHandler messageHandler, CommandsHandler commandsHandler) {
+    public Bot(SettingsHandler settingsHandler, PermissionHandler permissionHandler, MessageHandler messageHandler,
+               CommandsHandler commandsHandler, DataHandler dataHandler) {
         this.settingsHandler=settingsHandler;
         this.permissionHandler=permissionHandler;
         this.messageHandler=messageHandler;
         this.commandsHandler=commandsHandler;
+        this.dataHandler=dataHandler;
     }
 
     public SettingsHandler getSettingsHandler() {
@@ -45,4 +48,7 @@ public class Bot {
         return messageHandler;
     }
 
+    public DataHandler getDataHandler() {
+        return dataHandler;
+    }
 }

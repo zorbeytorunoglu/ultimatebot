8db247cd3f188b0a63edad789b426baa0f90c3d0
zorbeytorunoglu <thelegend.mcturk@gmail.com>
2022-06-08 02:08:53 +0100

    Added mute command

--- src/main/java/com/zorbeytorunoglu/ultimatebot/commands/CmdMute.java
+++ src/main/java/com/zorbeytorunoglu/ultimatebot/commands/CmdMute.java
@@ -1,9 +1,11 @@
 package com.zorbeytorunoglu.ultimatebot.commands;
 
 import com.zorbeytorunoglu.ultimatebot.Bot;
+import com.zorbeytorunoglu.ultimatebot.configuration.datas.Mute;
 import com.zorbeytorunoglu.ultimatebot.configuration.messages.Messages;
 import com.zorbeytorunoglu.ultimatebot.utils.BotUtils;
 import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.entities.Role;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 
 public class CmdMute implements MCPLCommand {
@@ -19,24 +21,19 @@ public class CmdMute implements MCPLCommand {
     @Override
     public void execute(Bot bot, MessageReceivedEvent event) {
         if (!event.isFromGuild()) return;
-        if (event.getAuthor().isBot()) return;
-
         if (!bot.getPermissionHandler().hasPermission("mute",event.getMember())) {
             event.getMessage().replyEmbeds(
                     BotUtils.getEmbed(messages.getNoPermissionTitle(),messages.getNoPermission(),messages.getNoPermissionColor())
             ).queue();
             return;
         }
-
         String[] args=event.getMessage().getContentRaw().split(" ");
-
         if (args.length==1) {
             event.getMessage().replyEmbeds(BotUtils.getEmbed(
                     messages.getMuteUsageTitle(),messages.getMuteUsage().replace("%command%",getCommand()),messages.getMuteUsageColor()
             )).queue();
             return;
         }
-
         Member member=BotUtils.getMember(event.getGuild(), event.getMessage().getMentions(), args[1]);
 
         if (member==null) {
@@ -45,13 +42,42 @@ public class CmdMute implements MCPLCommand {
             )).queue();
             return;
         }
+        if (args.length==2) {
 
+            if (BotUtils.getMute(event.getMember())!=null) {
+                event.getMessage().replyEmbeds(BotUtils.getEmbed(
+                        messages.getAlreadyMutedTitle(), messages.getAlreadyMuted().replace("%member%",member.getEffectiveName()),messages.getAlreadyMutedColor()
+                )).queue();
+                return;
+            }
 
+            Role mutedRole=BotUtils.getMutedRole(bot, event.getGuild());
+
+            if (!member.getGuild().getSelfMember().canInteract(mutedRole)) {
+                event.getMessage().replyEmbeds(BotUtils.getEmbed(
+                        messages.getMuteRoleIsPositionedHigherTitle(),messages.getMuteRoleIsPositionedHigherTitle(),messages.getMuteRoleIsPositionedHigherColor()
+                )).queue();
+                return;
+            }
+
+            Mute mute=new Mute(member.getIdLong());
+
+            Mute.getMutes().add(mute);
+
+            event.getGuild().addRoleToMember(member,mutedRole).queue();
+
+            event.getMessage().replyEmbeds(BotUtils.getEmbed(
+                    messages.getJustMutedTitle(),messages.getJustMuted().replace("%member%",member.getEffectiveName()),messages.getJustMutedColor()
+            )).queue();
+
+            return;
+
+        }
 
     }
 
     @Override
     public String getCommand() {
-        return null;
+        return bot.getCommandsHandler().getCommands().getPrefix()+bot.getCommandsHandler().getCommands().getMute();
     }
 }

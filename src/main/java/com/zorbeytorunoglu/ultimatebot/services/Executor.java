d672efbcfc7d72da32c0a803a67f06dea7d79639
zorbeytorunoglu <thelegend.mcturk@gmail.com>
2022-06-10 02:00:29 +0100

    Created Executor

---
+++ src/main/java/com/zorbeytorunoglu/ultimatebot/services/Executor.java
@@ -0,0 +1,35 @@
+package com.zorbeytorunoglu.ultimatebot.services;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+public class Executor {
+
+    private static ScheduledExecutorService scheduledExecutorService;
+
+    public static void init() {
+        scheduledExecutorService = Executors.newScheduledThreadPool(3);
+    }
+    
+    public static void scheduleRepeatingTask(Runnable runnable, int delay, int period, TimeUnit timeUnit) {
+        scheduledExecutorService.scheduleAtFixedRate(runnable,delay,period,timeUnit);
+    }
+
+    public static void scheduleDelayedTask(Runnable runnable, int delay, TimeUnit timeUnit) {
+        scheduledExecutorService.schedule(runnable,delay,timeUnit);
+    }
+
+    public static void scheduleRepeatingTask(Runnable runnable, int delay, int period) {
+        scheduledExecutorService.scheduleAtFixedRate(runnable,delay,period,TimeUnit.MILLISECONDS);
+    }
+
+    public static void scheduleDelayedTask(Runnable runnable, int delay) {
+        scheduledExecutorService.schedule(runnable,delay,TimeUnit.MILLISECONDS);
+    }
+
+    public static ScheduledExecutorService getScheduledExecutorService() {
+        return scheduledExecutorService;
+    }
+
+}

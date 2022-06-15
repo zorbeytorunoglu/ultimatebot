6097c8aa5dedf87df9ff13af2d23d18ef6ee3c59
zorbeytorunoglu <thelegend.mcturk@gmail.com>
2022-06-15 03:20:48 +0100

    Added TicketHandler

--- src/main/java/com/zorbeytorunoglu/ultimatebot/Bot.java
+++ src/main/java/com/zorbeytorunoglu/ultimatebot/Bot.java
@@ -4,6 +4,7 @@ import com.zorbeytorunoglu.ultimatebot.configuration.commands.CommandsHandler;
 import com.zorbeytorunoglu.ultimatebot.configuration.datas.DataHandler;
 import com.zorbeytorunoglu.ultimatebot.configuration.messages.MessageHandler;
 import com.zorbeytorunoglu.ultimatebot.configuration.settings.SettingsHandler;
+import com.zorbeytorunoglu.ultimatebot.configuration.tickets.TicketHandler;
 import com.zorbeytorunoglu.ultimatebot.permissions.PermissionHandler;
 import net.dv8tion.jda.api.JDA;
 
@@ -13,20 +14,24 @@ public class Bot {
     private final MessageHandler messageHandler;
     private final CommandsHandler commandsHandler;
     private final DataHandler dataHandler;
+    private final TicketHandler ticketHandler;
     private JDA jda;
 
     public Bot(SettingsHandler settingsHandler, PermissionHandler permissionHandler, MessageHandler messageHandler,
-               CommandsHandler commandsHandler, DataHandler dataHandler) {
+               CommandsHandler commandsHandler, DataHandler dataHandler, TicketHandler ticketHandler) {
         this.settingsHandler=settingsHandler;
         this.permissionHandler=permissionHandler;
         this.messageHandler=messageHandler;
         this.commandsHandler=commandsHandler;
         this.dataHandler=dataHandler;
+        this.ticketHandler=ticketHandler;
     }
 
     public SettingsHandler getSettingsHandler() {
         return settingsHandler;
     }
+
+    public TicketHandler getTicketHandler() { return ticketHandler; }
 
     public JDA getJda() {
         return jda;

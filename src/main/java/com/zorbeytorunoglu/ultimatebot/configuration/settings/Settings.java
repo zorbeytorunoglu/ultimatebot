78b1bffa19da6a1dfe06d640cbaf9c8125752545
zorbeytorunoglu <thelegend.mcturk@gmail.com>
2022-06-21 03:45:07 +0100

    Added welcome settings

--- src/main/java/com/zorbeytorunoglu/ultimatebot/configuration/settings/Settings.java
+++ src/main/java/com/zorbeytorunoglu/ultimatebot/configuration/settings/Settings.java
@@ -1,5 +1,13 @@
 package com.zorbeytorunoglu.ultimatebot.configuration.settings;
 
+import net.dv8tion.jda.api.Permission;
+import net.dv8tion.jda.api.entities.MessageEmbed;
+
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+
 public class Settings {
 
     private final SettingsContainer settingsContainer;
@@ -11,17 +19,13 @@ public class Settings {
     public String getToken() {
         return settingsContainer.token;
     }
-
     public String getActivity() {
         return settingsContainer.activity;
     }
-
     public String getActivityLabel() {
         return settingsContainer.activityLabel;
     }
-
     public String getMuteRoleName() { return settingsContainer.mutedRoleName; }
-
     public Integer getMaxMuteMinute() { return settingsContainer.maxMuteMinute; }
     public Integer getMaxPurgeNumber() { return settingsContainer.maxPurgeNumber; }
     public Integer getMaxSlowmode() { return settingsContainer.maxSlowmode; }
@@ -29,5 +33,26 @@ public class Settings {
     public Integer getWarnLimit() { return settingsContainer.warnLimit; }
     public String getWarnAction() { return settingsContainer.warnAction; }
     public Integer getMemberTicketLimit() { return settingsContainer.memberTicketLimit; }
+    public boolean getWelcomeEnabled() { return settingsContainer.welcomeEnabled; }
+    public Collection<Permission> getTicketMemberAddPermissions() {
+        String[] perms=settingsContainer.ticketAddMemberPerms.split(",");
+        if (perms.length==0) return Collections.EMPTY_LIST;
+        Collection<Permission> permissionCollection=new ArrayList<>();
+        for (String perm:perms) {
+            permissionCollection.add(Permission.valueOf(Permission.class,perm));
+        }
+        return permissionCollection;
+    }
+    public String getWelcomeEmbedAuthor() { return settingsContainer.welcomeEmbedAuthor; }
+    public String getWelcomeEmbedAuthorUrl() { return settingsContainer.welcomeEmbedAuthorUrl; }
+    public String getWelcomeEmbedAuthorImgUrl() { return settingsContainer.welcomeEmbedAuthorImgUrl; }
+    public String getWelcomeEmbedFooter() { return settingsContainer.welcomeEmbedFooter; }
+    public String getWelcomeEmbedFooterImgUrl() { return settingsContainer.welcomeEmbedFooterImgUrl; }
+    public String getWelcomeEmbedThumbnail() { return settingsContainer.welcomeEmbedThumbnailUrl; }
+    public Collection<MessageEmbed.Field> getWelcomeEmbedFields() { return settingsContainer.welcomeEmbedFields; }
+    public Color getWelcomeEmbedColor() { return settingsContainer.welcomeEmbedColor; }
+    public String getWelcomeTextChannelId() { return settingsContainer.welcomeTextChannel; }
+    public String getWelcomeEmbedDescription() { return settingsContainer.welcomeEmbedDescription; }
+    public String getWelcomeEmbedTitle() { return settingsContainer.welcomeEmbedTitle; }
 
 }
--- src/main/java/com/zorbeytorunoglu/ultimatebot/configuration/settings/SettingsContainer.java
+++ src/main/java/com/zorbeytorunoglu/ultimatebot/configuration/settings/SettingsContainer.java
@@ -1,6 +1,13 @@
 package com.zorbeytorunoglu.ultimatebot.configuration.settings;
 
 import com.zorbeytorunoglu.ultimatebot.configuration.Configuration;
+import com.zorbeytorunoglu.ultimatebot.utils.BotUtils;
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.MessageEmbed;
+
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.Collection;
 
 public class SettingsContainer {
 
@@ -15,8 +22,22 @@ public class SettingsContainer {
     public final Integer warnLimit;
     public final String warnAction;
     public final Integer memberTicketLimit;
+    public final String ticketAddMemberPerms;
+    public final boolean welcomeEnabled;
+    public final String welcomeEmbedTitle;
+    public final String welcomeEmbedDescription;
+    public final Color welcomeEmbedColor;
+    public final String welcomeEmbedFooter;
+    public final String welcomeEmbedFooterImgUrl;
+    public final String welcomeEmbedAuthor;
+    public final String welcomeEmbedAuthorUrl;
+    public final String welcomeEmbedAuthorImgUrl;
+    public final String welcomeEmbedThumbnailUrl;
+    public final Collection<MessageEmbed.Field> welcomeEmbedFields;
+    public final String welcomeTextChannel;
 
     public SettingsContainer(Configuration configuration) {
+
         this.token=configuration.getString("token");
         this.activity=configuration.getString("activity");
         this.activityLabel=configuration.getString("activity-label");
@@ -28,5 +49,27 @@ public class SettingsContainer {
         this.warnLimit=configuration.getInt("warns.limit");
         this.warnAction=configuration.getString("warns.action");
         this.memberTicketLimit=configuration.getInt("tickets.member-ticket-limit");
+        this.ticketAddMemberPerms=configuration.getString("tickets.member-add-perms");
+        this.welcomeEnabled=configuration.getBoolean("welcome.enabled");
+        this.welcomeEmbedTitle=configuration.getString("welcome.embed.title");
+        this.welcomeEmbedDescription=configuration.getString("welcome.embed.description");
+        this.welcomeEmbedFooter=configuration.getString("welcome.embed.footer");
+        this.welcomeEmbedColor=configuration.getString("welcome.embed.color").equals("none") ? Color.BLACK : BotUtils.getColor(configuration.getString("welcome.embed.color"));
+        this.welcomeEmbedFooterImgUrl=configuration.getString("welcome.embed.footer-img-url");
+        this.welcomeEmbedAuthor=configuration.getString("welcome.embed.author");
+        this.welcomeEmbedAuthorUrl=configuration.getString("welcome.embed.author-url");
+        this.welcomeEmbedAuthorImgUrl=configuration.getString("welcome.embed.author-img-url");
+        this.welcomeEmbedThumbnailUrl=configuration.getString("welcome.embed.thumbnail-img-url");
+        this.welcomeEmbedFields=new ArrayList<>();
+        if (configuration.getSection("welcome.embed.fields")!=null) {
+            Collection<String> keyCollection=configuration.getSection("welcome.embed.fields").getKeys();
+            for (String key:keyCollection) {
+                welcomeEmbedFields.add(new MessageEmbed.Field(configuration.getString("welcome.embed.fields."+key+".title"),
+                        configuration.getString("welcome.embed.fields."+key+".description"),
+                        configuration.getBoolean("welcome.embed.fields."+key+".inline")));
+            }
+        }
+        this.welcomeTextChannel=configuration.getString("welcome.channel")=="" ? null : configuration.getString("welcome.channel");
+
     }
 }

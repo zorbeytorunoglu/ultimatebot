6e76519909bb66193d1ff6b528b8a71b74e6decd
zorbeytorunoglu <thelegend.mcturk@gmail.com>
2022-06-21 03:44:12 +0100

    Added add&remove command, added welcome

--- src/main/java/com/zorbeytorunoglu/ultimatebot/MCPL.java
+++ src/main/java/com/zorbeytorunoglu/ultimatebot/MCPL.java
@@ -6,6 +6,8 @@ import com.zorbeytorunoglu.ultimatebot.commands.admin.CmdSetStatus;
 import com.zorbeytorunoglu.ultimatebot.commands.misc.CmdAnnounce;
 import com.zorbeytorunoglu.ultimatebot.commands.misc.CmdPing;
 import com.zorbeytorunoglu.ultimatebot.commands.moderation.*;
+import com.zorbeytorunoglu.ultimatebot.commands.ticket.CmdAdd;
+import com.zorbeytorunoglu.ultimatebot.commands.ticket.CmdRemove;
 import com.zorbeytorunoglu.ultimatebot.commands.ticket.CmdTicketPanel;
 import com.zorbeytorunoglu.ultimatebot.configuration.Resource;
 import com.zorbeytorunoglu.ultimatebot.configuration.commands.CommandsHandler;
@@ -15,6 +17,7 @@ import com.zorbeytorunoglu.ultimatebot.configuration.settings.SettingsHandler;
 import com.zorbeytorunoglu.ultimatebot.configuration.tickets.TicketHandler;
 import com.zorbeytorunoglu.ultimatebot.gui.GUI;
 import com.zorbeytorunoglu.ultimatebot.listeners.Ticket;
+import com.zorbeytorunoglu.ultimatebot.listeners.Welcome;
 import com.zorbeytorunoglu.ultimatebot.permissions.PermissionHandler;
 import com.zorbeytorunoglu.ultimatebot.services.Executor;
 import com.zorbeytorunoglu.ultimatebot.utils.BotUtils;
@@ -57,6 +60,7 @@ public class MCPL {
         builder.setActivity(BotUtils.getActivity(bot));
 
         builder.addEventListeners(new Ticket(bot),
+                new Welcome(bot),
 
                 new CommandListener(bot,
                 new CmdSetStatus(bot),
@@ -73,7 +77,9 @@ public class MCPL {
                 new CmdPurge(bot),
                 new CmdSlowmode(bot),
                 new CmdWarn(bot),
-                new CmdTicketPanel(bot)));
+                new CmdTicketPanel(bot),
+                        new CmdAdd(bot),
+                        new CmdRemove(bot)));
 
         JDA jda=builder.build();
 
